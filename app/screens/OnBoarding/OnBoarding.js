acc81c6f08cb0e9b40ff1179dc0a2124b38e44d4
Teddy Walter <teddywalter2016@gmail.com>
2021-09-30 02:39:29 +0200

    stack navigation for onboarding

---
+++ app/screens/OnBoarding/OnBoarding.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const OnBoarding = () => {
+  return (
+    <SafeAreaView>
+      <Text>HELLO</Text>
+    </SafeAreaView>
+  );
+};
+
+export default OnBoarding;
